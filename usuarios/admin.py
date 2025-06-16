75e26beb1ffdd5983e0185e75631a8ced682caa9
JoePinoOG <estay980@gmail.com>
2025-06-16 13:31:33 -0400

    solicitudes de registro panel admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Usuario
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'rol', 'estado', 'is_active')
+    list_filter = ('estado', 'rol')
+    actions = ['aprobar_usuarios', 'rechazar_usuarios']
+
+    def aprobar_usuarios(self, request, queryset):
+        queryset.update(estado='APROBADO')
+    aprobar_usuarios.short_description = "Aprobar usuarios seleccionados"
+
+    def rechazar_usuarios(self, request, queryset):
+        queryset.update(estado='RECHAZADO')
+    rechazar_usuarios.short_description = "Rechazar usuarios seleccionados"
