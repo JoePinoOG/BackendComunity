1ed9e49be44e995d9a72694defda339e25aafb42
JoePinoOG <estay980@gmail.com>
2025-06-16 14:37:59 -0400

    permitir registro de usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from .models import Usuario
 from .serializers import UsuarioSerializer, AprobarUsuarioSerializer
 from .permissions import EsSecretario, EsDirectiva
@@ -8,6 +9,7 @@ from .permissions import EsSecretario, EsDirectiva
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+    permission_classes = [AllowAny] 
 
     @action(detail=True, methods=['post'], permission_classes=[EsSecretario])
     def aprobar(self, request, pk=None):
