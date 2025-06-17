7e74ea3161ff4d92566db8af3bd99fdfdf533a74
JoePinoOG <estay980@gmail.com>
2025-06-17 01:59:17 -0400

    quitando el estatus de aprobacion para realizar pruebas

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Usuario
-from .serializers import UsuarioSerializer, AprobarUsuarioSerializer
+from .serializers import UsuarioSerializer
 from .permissions import EsSecretario, EsDirectiva
 from rest_framework.views import APIView
 
